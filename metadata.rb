f40dead8f480a5532a10592815d93eeb7c612bd6
Darren <wdarrenc@hotmail.com>
2014-01-20 13:23:56 -0600

    Initial Push

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'useradd'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'wdarrenc@hotmail.com'
+license          'GNU Public License 3.0'
+description      'Installs/Configures useradd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
